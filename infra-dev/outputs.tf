3217899a98e8dad4d77e6604f99fe3e614a2592d
Matt Hoyle <matt.hoyle@xealth.io>
2022-10-21 10:15:02 -0400

    Test pr

--- infra-dev/outputs.tf
+++ infra-dev/outputs.tf
@@ -16,7 +16,7 @@ output "website_bucket_arn" {
 }
 
 output "website_bucket_name" {
-  description = "Name (id) of the bucket"
+  description = "Name id) of the bucket"
   value       = module.website_s3_bucket.name
 }
 
