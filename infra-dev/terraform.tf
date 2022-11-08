aeaa73111fe61b9b36948b2b776eadad43300ac1
Matt Hoyle <matt.hoyle@xealth.io>
2022-11-08 12:40:46 -0500

    Switch state backend to s3 from tfcloud

---
+++ infra-dev/terraform.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = "~>0.12.0"
+
+  backend "s3" {
+    encrypt        = "true"
+    bucket         = "xealth-demo-tfstate"
+    key            = "staging/infra-dev/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
